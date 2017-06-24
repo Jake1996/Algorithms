898794502ebdd81603cf922fd6b92182b137f5e0
Jake1996 <jatinpm96@gmail.com>
2017-06-24 13:25:06 +0530

    Lines and dots

--- Geometry/LinesAndDots.java
+++ Geometry/LinesAndDots.java
@@ -1,6 +1,43 @@
 package Geometry;
 
+import java.util.ArrayList;
+
 public class LinesAndDots {
-	
-	
+
+    public static class Point2D {
+        double x, y;
+
+        public Point2D(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int compareTo(Point2D b) {
+            if(x>b.x) return 1;
+            else if(x<b.x) return -1;
+            else if(y>b.y) return 1;
+            else if(y<b.y) return -1;
+            return 0;
+        }
+
+        public static double distanceBTW(Point2D a, Point2D b) {
+            double x = a.x - b.y;
+            double y = a.y - b.y;
+            return Math.sqrt(x * x + y * y);
+        }
+
+        public double distanceTo(Point2D a) {
+            double tx = a.x - x;
+            double ty = a.y - y;
+            return Math.sqrt(tx * tx + ty * ty);
+        }
+
+        public static double shortestDistance(ArrayList<Point2D> arr) {
+            arr.sort((Point2D a,Point2D b)->a.compareTo(b));
+            
+            
+            return 0;
+        }
+
+    }
 }
---
+++ Tester/geometry.java
@@ -0,0 +1,15 @@
+package Tester;
+
+import java.util.ArrayList;
+
+import Geometry.LinesAndDots;
+
+public class geometry {
+    public static void main(String args[]) {
+        LinesAndDots.Point2D a = new LinesAndDots.Point2D(1, 2);
+        LinesAndDots.Point2D b = new LinesAndDots.Point2D(2, 1);
+        ArrayList<LinesAndDots.Point2D> arr = new ArrayList<>();
+        arr.add(a);
+        arr.add(b);
+    }
+}
