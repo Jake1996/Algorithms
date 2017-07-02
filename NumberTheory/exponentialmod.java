cfea58c4450909cd1bda8b426e16ebe7332d9e74
Jake1996 <jatinpm96@gmail.com>
2017-07-02 18:39:05 +0530

    correction to exponential mod

--- NumberTheory/exponentialmod.java
+++ NumberTheory/exponentialmod.java
@@ -3,6 +3,7 @@ package NumberTheory;
 public class exponentialmod {
 	public static long apowbmodc(long a,long b,long c) {
 		long ans = 1;
+		a%=c;
 		while(b!=0) {
 			if( (b&1) == 1 ) {
 				ans = ans*a;
